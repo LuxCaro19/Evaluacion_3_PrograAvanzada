b3ee2f2f6785901d64410caf4d01f654ffa08e1c
LuxCaro19 <luiscaroretamal2000@gmail.com>
2021-07-09 02:52:43 -0400

    Agregar archivos de proyecto.

---
+++ Evaluacion_THA/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Evaluacion_THA.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Interna()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Externa()
+        {
+            return View();
+        }
+    }
+}
---
+++ Evaluacion_THA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Evaluacion_THA.MvcApplication" Language="C#" %>
